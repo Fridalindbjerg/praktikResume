68a2310d5558d5e6b1206bd1b333cca9fa600d94
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2025-09-28 17:56:06 +0200

    adds js + fonts etc

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-window.addEventListener("load", start);
+// window.addEventListener("load", start);
 
-function start() {
-  console.log("start");
-  document.querySelector("h2").classList.add("build");
+// function start() {
+//   console.log("start");
+//   document.querySelector("h2").classList.add("build");
+// }
+
+const text = "Multimediedesign Studerende              ";
+// const speed = 90; // millisekunder mellem hvert bogstav
+// let index = 0;
+
+// function typeWriter() {
+//   if (index < text.length) {
+//     document.getElementById("typewriter").textContent += text.charAt(index);
+//     index++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", typeWriter);
+
+const speed = 110;
+let index = 0;
+
+function typeWriter() {
+  console.log("typewriter works");
+  if (index < text.length) {
+    document.getElementById("text").textContent += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", typeWriter);
